edf683eef51ed4e91c7c3fc1eb0ebed8a0397902
Alan <lic.sanchezalan@gmail.com>
2024-09-19 21:19:36 -0300

    new desing and functions

--- JS/aprendizaje.js
+++ JS/aprendizaje.js
@@ -8,6 +8,9 @@ const btn1Msg = document.getElementById("btn1-msg")
 const btn2Msg = document.getElementById("btn2-msg")
 const btn3Msg = document.getElementById("btn3-msg")
 const btn4Msg = document.getElementById("btn4-msg")
+const gamesBtn = document.getElementById("games-btn")
+const inputGames = document.getElementById("input-games")
+const formPlayers = document.getElementById("formPlayers")
 
 
 // Misma respuesta al evento click para cada botón: 
@@ -38,3 +41,7 @@ btn4.addEventListener('click', () => {
     btn2Msg.style.display="none"
     btn3Msg.style.display="none"
 });
+
+gamesBtn.addEventListener('click', () => {
+    window.open('https://www.365chess.com/chess-games.php?wlname=' + inputGames.value, '_blank');
+})
